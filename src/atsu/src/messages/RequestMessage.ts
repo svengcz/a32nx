eab11380ea2958f8d0a13aadcf48995248a6e4a5
Sven Czarnian <devel@svcz.de>
2022-02-04 20:18:38 +0100

    introduce a request message

---
+++ src/atsu/src/messages/RequestMessage.ts
@@ -0,0 +1,82 @@
+//  Copyright (c) 2021 FlyByWire Simulations
+//  SPDX-License-Identifier: GPL-3.0
+
+import { AtsuMessageType, AtsuMessageSerializationFormat, AtsuMessageDirection } from './AtsuMessage';
+import { CpdlcMessage, CpdlcMessageResponse } from './CpdlcMessage';
+import { cpdlcToString, wordWrap } from '../Common';
+
+/**
+ * Defines the general CPDLC request message
+ */
+export class RequestMessage extends CpdlcMessage {
+    public Request: string = '';
+
+    public Freetext0: string = '';
+
+    public Freetext1: string = '';
+
+    public Freetext2: string = '';
+
+    public Freetext3: string = '';
+
+    public Freetext4: string = '';
+
+    constructor() {
+        super();
+        this.Type = AtsuMessageType.Request;
+        this.Direction = AtsuMessageDirection.Output;
+    }
+
+    public deserialize(jsonData: any): void {
+        super.deserialize(jsonData);
+
+        this.Request = jsonData.Request;
+        this.Freetext0 = jsonData.Freetext0;
+        this.Freetext1 = jsonData.Freetext1;
+        this.Freetext2 = jsonData.Freetext2;
+        this.Freetext3 = jsonData.Freetext3;
+        this.Freetext4 = jsonData.Freetext4;
+    }
+
+    public serialize(format: AtsuMessageSerializationFormat) {
+        let content = this.Request;
+        let message = '';
+        let freetext = [
+            this.Freetext0,
+            this.Freetext1,
+            this.Freetext2,
+            this.Freetext3,
+            this.Freetext4,
+        ];
+        freetext = freetext.filter((n) => n);
+        content += freetext.join(' ');
+        const lines = wordWrap(content, 25);
+
+        if (format === AtsuMessageSerializationFormat.Network) {
+            message = `/data2/${this.CurrentTransmissionId}/${this.PreviousTransmissionId !== -1 ? this.PreviousTransmissionId : ''}/${cpdlcToString(this.RequestedResponses)}`;
+            message += `/${content}`;
+        } else if (format === AtsuMessageSerializationFormat.DCDU) {
+            message = lines.join('\n');
+        } else if (format === AtsuMessageSerializationFormat.MCDU) {
+            message += `{cyan}${this.Timestamp.dcduTimestamp()} TO ${this.Station}{end}\n`;
+            message += lines.join('\n');
+            message += '{white}------------------------{end}\n';
+
+            if (this.ResponseType === CpdlcMessageResponse.Other && this.Response !== undefined) {
+                message += this.Response.serialize(format);
+            }
+        } else if (format === AtsuMessageSerializationFormat.Printer) {
+            message += `${this.Timestamp.dcduTimestamp()} TO ${this.Station}}\n`;
+            message += lines.join('\n');
+            message += '------------------------\n';
+
+            if (this.ResponseType === CpdlcMessageResponse.Other && this.Response !== undefined) {
+                message += this.Response.serialize(format);
+            }
+        } else {
+            message = content;
+        }
+
+        return message;
+    }
+}

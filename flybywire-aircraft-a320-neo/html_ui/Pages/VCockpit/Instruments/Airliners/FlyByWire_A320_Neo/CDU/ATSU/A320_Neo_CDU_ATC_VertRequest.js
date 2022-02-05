a3e2a527939b78b5f0fde728265a7c2fa29e287f
Sven Czarnian <devel@svcz.de>
2022-02-05 13:03:14 +0100

    introduce the vertical request

---
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_VertRequest.js
@@ -0,0 +1,133 @@
+class CDUAtcVertRequest {
+    static ShowPage1(mcdu, dataSet = false) {
+        mcdu.clearDisplay();
+
+        if (mcdu.requestMessage !== undefined && !dataSet) {
+            mcdu.requestMessage = undefined;
+        }
+
+        mcdu.setTemplate([
+            ["ATC VERT REQ", "1", "2"],
+            ["\xa0CLB TO/START AT", "ALT\xa0"],
+            ["[   ]/[   ][color]cyan", "[   ][color]cyan"],
+            ["\xa0DES TO/START AT", "SPD\xa0"],
+            ["[   ]/[   ][color]cyan", "[ ][color]cyan"],
+            ["---WHEN CAN WE EXPECT---"],
+            ["{cyan}{{end}HIGHER ALT", "LOWER ALT{cyan}}{end}"],
+            ["", "WHEN CAN SPD\xa0"],
+            ["", "[ ][color]cyan"],
+            ["\xa0ALL FIELDS"],
+            ["\xa0ERASE", "ADD TEXT>"],
+            ["\xa0ATC MENU", "ATC\xa0[color]cyan"],
+            ["<RETURN", "REQ DISPLAY\xa0[color]cyan"]
+        ]);
+
+        mcdu.leftInputDelay[4] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[4] = () => {
+            CDUAtcVertRequest.ShowPage1(mcdu, false);
+        };
+
+        mcdu.leftInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[5] = () => {
+            CDUAtcMenu.ShowPage1(mcdu);
+        };
+
+        mcdu.rightInputDelay[4] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[4] = () => {
+            // TODO
+            //if (dataSet) {
+            //    CDUAtcLatRequest.CreateMessage(mcdu, dir, wxDev, sid, offset, offsetStart, hdg, trk, backOnTrack);
+            //}
+            CDUAtcText.ShowPage1(mcdu, "REQ", false);
+        };
+
+        mcdu.rightInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[5] = () => {
+            if (dataSet) {
+                // TODO
+                //CDUAtcLatRequest.CreateMessage(mcdu, dir, wxDev, sid, offset, offsetStart, hdg, trk, backOnTrack);
+                mcdu.atsuManager.registerMessage(mcdu.requestMessage);
+                mcdu.requestMessage = undefined;
+                CDUAtcVertRequest.ShowPage1(mcdu, false);
+            }
+        };
+
+        mcdu.onNextPage = () => {
+            CDUAtcVertRequest.ShowPage2(mcdu, parent, false);
+        };
+    }
+
+    static ShowPage2(mcdu, dataSet = false) {
+        mcdu.clearDisplay();
+
+        if (mcdu.requestMessage !== undefined && !dataSet) {
+            mcdu.requestMessage = undefined;
+        }
+
+        mcdu.setTemplate([
+            ["ATC VERT REQ", "2", "2"],
+            ["\xa0BLOCK ALT", "VMC\xa0"],
+            ["[   ]/[   ][color]cyan", "DESCENT{cyan}}{end}"],
+            ["\xa0CRZ CLB TO", "SPD RANGE\xa0"],
+            ["[   ][color]cyan", "[ ]/[ ][color]cyan"],
+            [""],
+            ["{small}---WHEN CAN WE EXPECT---{end}"],
+            ["\xa0CRZ CLB TO", "SPD RANGE\xa0"],
+            ["[   ][color]cyan", "[ ]/[ ][color]cyan"],
+            ["\xa0ALL FIELDS"],
+            ["\xa0ERASE", "ADD TEXT>"],
+            ["\xa0ATC MENU", "ATC\xa0[color]cyan"],
+            ["<RETURN", "REQ DISPLAY\xa0[color]cyan"]
+        ]);
+
+        mcdu.leftInputDelay[4] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[4] = () => {
+            CDUAtcVertRequest.ShowPage1(mcdu, false);
+        };
+
+        mcdu.leftInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[5] = () => {
+            CDUAtcMenu.ShowPage1(mcdu);
+        };
+
+        mcdu.rightInputDelay[4] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[4] = () => {
+            // TODO
+            //if (dataSet) {
+            //    CDUAtcLatRequest.CreateMessage(mcdu, dir, wxDev, sid, offset, offsetStart, hdg, trk, backOnTrack);
+            //}
+            CDUAtcText.ShowPage1(mcdu, "REQ", false);
+        };
+
+        mcdu.rightInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[5] = () => {
+            if (dataSet) {
+                // TODO
+                //CDUAtcLatRequest.CreateMessage(mcdu, dir, wxDev, sid, offset, offsetStart, hdg, trk, backOnTrack);
+                mcdu.atsuManager.registerMessage(mcdu.requestMessage);
+                mcdu.requestMessage = undefined;
+                CDUAtcVertRequest.ShowPage1(mcdu, false);
+            }
+        };
+
+        mcdu.onPrevPage = () => {
+            CDUAtcVertRequest.ShowPage1(mcdu, parent, false);
+        };
+    }
+}

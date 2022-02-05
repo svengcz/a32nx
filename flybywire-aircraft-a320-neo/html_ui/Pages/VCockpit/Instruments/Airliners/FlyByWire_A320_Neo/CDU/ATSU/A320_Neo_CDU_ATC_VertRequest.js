526a810720ed9d5a1e8caf8871e056c4bc4896fb
Sven Czarnian <devel@svcz.de>
2022-02-05 15:13:51 +0100

    add functions to create a datablock and to validate and format altitudes and speeds

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_VertRequest.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_VertRequest.js
@@ -1,5 +1,137 @@
 class CDUAtcVertRequest {
-    static ShowPage1(mcdu, dataSet = false) {
+    static CreateDataBlock() {
+        return {
+            clb: null,
+            des: null,
+            startAt: null,
+            alt: null,
+            spd: null,
+            whenHigher: false,
+            whenLower: false,
+            whenSpd: false,
+            blockAltLow: null,
+            blockAltHigh: null,
+            cruise: null,
+            spdLow: null,
+            spdHigh: null,
+            whenCruise: false,
+            whenSpdRange: false
+        };
+    }
+
+    static CanSendData(data) {
+        return data.clb || data.des || data.startAt || data.alt || data.spd || data.whenHigher || data.whenLower ||
+            data.whenSpd || data.blockAltLow || data.blockAltHigh || data.cruise || data.spdLow || data.spdHigh ||
+            data.whenCruise || data.whenSpdRange;
+    }
+
+    static ValidateAltitude(value) {
+        if (/^((FL)*[0-9]{1,3})$/.test(value)) {
+            let flightlevel = "";
+
+            if (value.startsWith("FL")) {
+                flightlevel = value.substring(2, value.length);
+            } else {
+                flightlevel = value;
+            }
+
+            // contains not only digits
+            if (/(?!^\d+$)^.+$/.test(flightlevel)) {
+                return NXSystemMessages.formatError;
+            }
+            flightlevel = parseInt(flightlevel);
+
+            if (flightlevel >= 30 && flightlevel <= 410) {
+                return null;
+            }
+            return NXSystemMessages.entryOutOfRange;
+        } else if (/^([0-9]{1,3}(FT|M)|[0-9]{1,5}M|[0-9]{4,5})$/.test(value)) {
+            const feet = value[value.length - 1] !== "M";
+
+            let altitude = value.replace("FT", "").replace("M", "");
+
+            // contains not only digits
+            if (/(?!^\d+$)^.+$/.test(altitude)) {
+                return NXSystemMessages.formatError;
+            }
+            altitude = parseInt(altitude);
+
+            if (feet) {
+                if (altitude >= 0 && altitude <= 25000) {
+                    return null;
+                }
+                return NXSystemMessages.entryOutOfRange;
+            }
+
+            if (altitude >= 0 && altitude <= 12500) {
+                return null;
+            }
+            return NXSystemMessages.entryOutOfRange;
+        }
+
+        return NXSystemMessages.formatError;
+    }
+
+    static FormatAltitude(value) {
+        if (/^((FL)*[0-9]{1,3})$/.test(value)) {
+            if (value.startsWith("FL")) {
+                return value;
+            } else {
+                return `FL${value}`;
+            }
+        } else if (/^([0-9]{1,3}(FT|M)|[0-9]{1,5}M|[0-9]{4,5})$/.test(value)) {
+            const feet = value[value.length - 1] !== "M";
+
+            let altitude = value.replace("FT", "").replace("M", "");
+            if (!feet) {
+                altitude = `${altitude}M`;
+            }
+
+            return altitude;
+        }
+
+        return "";
+    }
+
+    static ValidateSpeed(value) {
+        if (/^((M*)\.[0-9]{1,2})$/.test(value)) {
+            // MACH number
+
+            let mach = value.split(".")[1];
+            // contains not only digits
+            if (/(?!^\d+$)^.+$/.test(mach)) {
+                return NXSystemMessages.formatError;
+            }
+            mach = parseInt(mach);
+
+            if (mach >= 61 && mach <= 92) {
+                return null;
+            }
+            return NXSystemMessages.entryOutOfRange;
+        } else if (/^([0-9]{1,3}(KT)*)$/.test(value)) {
+            // knots
+
+            let knots = value.replace("KT", "");
+            // contains not only digits
+            if (/(?!^\d+$)^.+$/.test(knots)) {
+                return NXSystemMessages.formatError;
+            }
+            knots = parseInt(knots);
+
+            if (knots >= 70 && knots <= 350) {
+                return null;
+            }
+            return NXSystemMessages.entryOutOfRange;
+        }
+
+        return NXSystemMessages.formatError;
+    }
+
+    static FormatSpeed(value) {
+        // remove preceeding M and succeeding KT
+        return value.replace("M", "").replace("KT", "");
+    }
+
         mcdu.clearDisplay();
 
         if (mcdu.requestMessage !== undefined && !dataSet) {

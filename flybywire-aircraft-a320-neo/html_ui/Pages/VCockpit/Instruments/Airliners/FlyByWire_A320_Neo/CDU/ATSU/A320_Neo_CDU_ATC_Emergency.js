ea4b0b2ba51f5f5f011fd5f26d2f280c69e9d419
Sven Czarnian <devel@svcz.de>
2022-02-06 19:58:29 +0100

    update the emergency page to MSN7560

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_Emergency.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_Emergency.js
@@ -1,73 +1,28 @@
 class CDUAtcEmergency {
-    static ShowPage(mcdu, store = {"emergType": 0, "emergAds": 0, "des": "", "div": "", "freq": ""}) {
+    static ShowPage1(mcdu) {
         mcdu.clearDisplay();
-        let mayday = "<MAYDAY";
-        let panpan = "<PANPAN";
-        let ads = "EMERG ADS:\xa0";
-        let setAds = "SET\xa0";
-
-        if (store["emergType"] == 0) {
-            mayday += "[color]cyan";
-        } else {
-            panpan += "[color]cyan";
-        }
-        if (store["emergAds"] == 0) {
-            ads += "OFF";
-            setAds += "ON";
-        } else {
-            ads += "ON";
-            setAds += "OFF";
-        }
-        if (store["des"] == "") {
-            store["des"] = "_____[color]amber";
-        }
-        if (store["div"] == "") {
-            store["div"] = "____/____[color]amber";
-        }
-        if (store["freq"] == "") {
-            store["freq"] = "[121.5][color]cyan";
-        }
 
         mcdu.setTemplate([
-            ["EMERGENCY[color]amber"],
-            ["", ads + "\xa0"],
-            [mayday, setAds + "*[color]cyan"],
+            ["EMERGENCY[color]amber", "1", "2"],
+            ["", "EMERG ADS-C:OFF\xa0"],
+            ["{cyan}{{end}MAYDAY", "SET ON*[color]inop"],
             ["", "DESCENDING TO\xa0"],
-            [panpan, store["des"]],
-            ["", "DIVERTING VIA\xa0"],
-            ["", store["div"]],
-            ["\xa0VOICE[color]cyan", "FREQ\xa0"],
-            ["\xa0CONTACT[color]cyan", store["freq"]],
+            ["{cyan}{{end}PANPAN", "[    ][color]cyan"],
+            ["", "DIVERTING/VIA\xa0"],
+            ["", "[   ]/[   ][color]cyan"],
+            ["\xa0VOICE", "FREQ\xa0"],
+            ["{cyan}{{end}CONTACT------", "[        ][color]cyan"],
             ["\xa0ALL FIELDS"],
-            ["*ERASE"],
-            ["\xa0ATC MENU", "ATC\xa0[color]inop"],
-            ["<RETURN", "EMERG DISPL\xa0[color]inop"]
+            ["*ERASE", "ADD TEXT>"],
+            ["\xa0ATC MENU", "ATC\xa0[color]cyan"],
+            ["<RETURN", "EMERG DISPL*[color]cyan"]
         ]);
-
-        mcdu.leftInputDelay[0] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onLeftInput[0] = () => {
-            store["emergType"] = 0;
-            CDUAtcEmergency.ShowPage(mcdu, store);
-        };
-
-        mcdu.leftInputDelay[1] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onLeftInput[1] = () => {
-            store["emergType"] = 1;
-            CDUAtcEmergency.ShowPage(mcdu, store);
-        };
 
         mcdu.leftInputDelay[4] = () => {
             return mcdu.getDelaySwitchPage();
         };
         mcdu.onLeftInput[4] = () => {
-            store["des"] = "";
-            store["div"] = "";
-            store["freq"] = "";
-            CDUAtcEmergency.ShowPage(mcdu, store);
+            CDUAtcEmergency.ShowPage(mcdu);
         };
 
         mcdu.leftInputDelay[5] = () => {
@@ -77,46 +32,46 @@ class CDUAtcEmergency {
             CDUAtcMenu.ShowPage1(mcdu);
         };
 
-        mcdu.rightInputDelay[0] = () => {
+        mcdu.onNextPage = () => {
+            CDUAtcEmergency.ShowPage2(mcdu);
+        };
+    }
+
+    static ShowPage2(mcdu) {
+        mcdu.clearDisplay();
+
+        mcdu.setTemplate([
+            ["EMERGENCY[color]amber", "2", "2"],
+            ["\xa0CLBING TO", "OFFSETTING\xa0"],
+            ["[    ][color]cyan", "[  ][color]cyan"],
+            ["\xa0SOULS", "ENDURANCE\xa0"],
+            ["[ ][color]cyan", "[     ][color]cyan"],
+            [""],
+            ["{cyan}{{end}EMERGENCY CANCEL", ""],
+            [""],
+            [""],
+            ["\xa0ALL FIELDS"],
+            ["*ERASE", "ADD TEXT>"],
+            ["\xa0ATC MENU", "ATC\xa0[color]cyan"],
+            ["<RETURN", "EMERG DISPL*[color]cyan"]
+        ]);
+
+        mcdu.leftInputDelay[4] = () => {
             return mcdu.getDelaySwitchPage();
         };
-        mcdu.onRightInput[0] = () => {
-            if (store["emergAds"] == 0) {
-                store["emergAds"] = 1;
-            } else {
-                store["emergAds"] = 0;
-            }
-            CDUAtcEmergency.ShowPage(mcdu, store);
+        mcdu.onLeftInput[4] = () => {
+            CDUAtcEmergency.ShowPage(mcdu);
         };
 
-        mcdu.rightInputDelay[1] = () => {
+        mcdu.leftInputDelay[5] = () => {
             return mcdu.getDelaySwitchPage();
         };
-        mcdu.onRightInput[1] = (value) => {
-            if (value != "") {
-                store["des"] = value + "[color]cyan";
-            }
-            CDUAtcEmergency.ShowPage(mcdu, store);
+        mcdu.onLeftInput[5] = () => {
+            CDUAtcMenu.ShowPage1(mcdu);
         };
 
-        mcdu.rightInputDelay[2] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onRightInput[2] = (value) => {
-            if (value != "") {
-                store["div"] = value + "[color]cyan";
-            }
-            CDUAtcEmergency.ShowPage(mcdu, store);
-        };
-
-        mcdu.rightInputDelay[3] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onRightInput[3] = (value) => {
-            if (value != "") {
-                store["freq"] = "[" + value + "][color]cyan";
-            }
-            CDUAtcEmergency.ShowPage(mcdu, store);
+        mcdu.onPrevPage = () => {
+            CDUAtcEmergency.ShowPage1(mcdu);
         };
     }
 }
